0b46445ebe62244ddc4551aaf62d5528b91b1fe9
Adrian M.A <adrian@neurocadi.es>
2020-08-20 22:18:02 +0200

    Updated profile data (profession and city added)

--- src/components/MessagesWindow/MessagesWindow.jsx
+++ src/components/MessagesWindow/MessagesWindow.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react';
-import { getAllMessages } from '../../services/redux/actions.js';
+import React, {useEffect, useState } from 'react';
+import { getAllMessages, searchedMessages, writeMessage } from '../../services/redux/actions.js';
 import { connect } from 'react-redux';
 
 import './MessagesWindow.css';
@@ -10,20 +10,64 @@ const MessagesWindow = props => {
     
     useEffect(() => {
         getAllMessages();
+       
     },[]);
+    
+    const [publicWindow, setPublicWindow] = useState(true);
+    const [searchWindow, setSearchWindow] = useState(false);
+
+    const searchMessages = (event) => {
+        event.preventDefault();
+        const searchinput =  event.target.searchMessage.value;
+        searchedMessages(searchinput);
+        setPublicWindow(false);
+        setSearchWindow(true);
+    }
+
+    const allMessages = () => {
+        getAllMessages();
+        setSearchWindow(false);
+        setPublicWindow(true);
+    }
+
+    const newMessage = (event) => {
+        event.preventDefault();
+        const body = {
+            header: event.target.writeMessageHeader.value,
+            body: event.target.writeMessageBody.value,
+            name: props.user.name,
+            surnames: props.user.surnames,
+            ownerId: props.user._id,
+            imagesPath: props.user.imagesPath[0],
+            socialGroup: 'public'
+        }
+        writeMessage(body);
+        event.target.writeMessageHeader.value = "";
+        event.target.writeMessageBody.value = "";
+    }
 
     return(
         <div id="messagesWindow">
             <div id="header">
                 <label id="labelMessages">Mensajes:</label>
-                <input id="searchInputMessages" type="text" name="search" placeholder="Buscar mensaje..."/>
-                <button id="searchButtonMessages" type="submit">Buscar</button>
+                <form id="searchForm" onSubmit={searchMessages}>
+                    <input id="searchInputMessages" type="text" name="searchMessage" placeholder="Buscar mensaje..."/>
+                    <button id="searchButtonMessages" type="submit">Buscar</button>
+                </form>
+                <button id="searchButtonMessages" onClick={allMessages}>Todos</button>
             </div>
             <div id="messagesContent">
-            {props.publicMessages?.map(message => <Message key={message._id} message={message} />)}
+            {publicWindow && props.messages?.map(message => <Message key={message._id} message={message} />)}
+            {searchWindow && props.messages?.map(message => <Message key={message._id} message={message} />)}
             </div>
+            <form id="writeMessage" onSubmit={newMessage}>
+                <input id="writeMessageTitle" type="text" name="writeMessageHeader" placeholder="Título del mensaje..."/>
+                <input id="writeMessageBody" type="text" name="writeMessageBody" placeholder="Escribe aquí tu mensaje..."/>
+                <button id="writeMessagesButton" type="submit">Enviar mensaje</button>
+            </form>
         </div>
     )
 }
-const mapStateToProps = ({publicMessages}) => ({publicMessages: publicMessages});
+
+const mapStateToProps = (state) => ({messages: state.messages, user: state.user});
 export default connect (mapStateToProps)(MessagesWindow);
